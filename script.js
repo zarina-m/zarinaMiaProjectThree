fbcc0553681d654c5c380f19bccf9539dcb84745
Zarina M <zarinabliss@gmail.com>
2020-04-13 02:27:17 -0400

    added assets folder and finished media queries

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Create namespace
 const app = {}
-
+//Keep track of selected date
 app.currentMonth = "";
 app.currentDay = "";
-
+//Used to loop between previous and next months 
 app.monthLookup = [
     "January",
     "February",
@@ -18,7 +18,7 @@ app.monthLookup = [
     "November",
     "December"
 ]
-//Create obj to hold calendar information and saved tasks
+//Create object to hold calendar information and saved tasks
 app.calendarLibrary = {
     January: {
         numDays: 31,
@@ -80,7 +80,6 @@ app.calendarLibrary = {
         blankColumns: 2,
         dailyTodo: [],
     },
-
 }
 
 //Load selected month
@@ -99,20 +98,23 @@ app.loadMonth = function(month) {
     $('h2').text(`${month}`);
 
     app.currentMonth = month;
-
+    //Load highlighted days 
     app.highlightDays()
 }
 
+//Function to highlight days with todo entries
 app.highlightDays = function() {
     for (let i = 0; i < app.calendarLibrary[app.currentMonth]['dailyTodo'].length; i++) {
         let todo = app.calendarLibrary[app.currentMonth]['dailyTodo'][i];
         if (todo.length !== 0) {
-            $("#" + (i + 1)).addClass('event')
+            $("#" + (i + 1)).addClass('containsTodoItem')
         } else {
-            $("#" + (i + 1)).removeClass('event')
+            $("#" + (i + 1)).removeClass('containsTodoItem')
         }
     } 
 }
+
+//Function to delete todo item
 app.deleteItem = function (e) {
 
     let todoText = "";
@@ -131,46 +133,47 @@ app.deleteItem = function (e) {
     if (index !== -1) {
         app.calendarLibrary[app.currentMonth]['dailyTodo'][app.currentDay].splice(index, 1);
     }
-
+    //After changing calendar library object, save to local storage 
     localStorage.setItem('calendarLibrary', JSON.stringify(app.calendarLibrary));
     app.highlightDays()
 }
 
+//Initialize app and set up event handlers 
 app.init = function() {
 
     for (let month in app.calendarLibrary) {
         for (let i = 0; i < app.calendarLibrary[month]['numDays']; i++) {
-            // Add {numDays} arrays to this
+            // Add {numDays} arrays to current month
             app.calendarLibrary[month]['dailyTodo'].push([])
         }
     }
-
+    //Check if local storage contains calendarLibrary
     localCalendarLibrary = localStorage.getItem('calendarLibrary');
     if (localCalendarLibrary !== null) {
         app.calendarLibrary = JSON.parse(localCalendarLibrary);
     }
-
+    //Start app with April calendar
     app.loadMonth('April');
-
+    //Handles clicks on days 
     $('.gridDay').on('click', function(e) {
         $('.agenda').show()
         $('.calendar').addClass('removeBottomBorder')
         app.currentDay = parseInt(e.target.innerText) - 1;
 
-        //TODO: get items for this day and display
+        //Get items for this day and display
         $('ul').empty()
         let listItems = app.calendarLibrary[app.currentMonth]['dailyTodo'][app.currentDay];
         for (let i = 0; i < listItems.length; i++) {
             $('ul').append(`<li><i class='fas fa-star'></i>${listItems[i]}<button class='deleteItem'><i class='far fa-times-circle'></i> </button> </li>`);
         }
-        // Remove deleteItem click handler and then add deleteItem click handler
-        // We do this so the event handler is only called once
+
+        // Remove deleteItem click handler and then add deleteItem click handler so it's only called once
         $('.deleteItem').off('click').on('click', app.deleteItem)
         //Add date above input
         $('label').html(`Enter an event below to add it to <span>${app.currentMonth} ${app.currentDay + 1}</span>`);
     })
 
-
+    //Add todo item to currently selected day 
     $('form').on('submit', function (e) {
         e.preventDefault();
         let listItem = $('input').val().trim(' ');
@@ -182,21 +185,18 @@ app.init = function() {
             app.calendarLibrary[app.currentMonth]['dailyTodo'][app.currentDay].push(listItem);
         }
 
-        // Remove deleteItem click handler and then add deleteItem click handler
-        // We do this so the event handler is only called once
+        // Remove deleteItem click handler and then add deleteItem click handler so it's only called once
         $('.deleteItem').off('click').on('click', app.deleteItem)
         localStorage.setItem('calendarLibrary', JSON.stringify(app.calendarLibrary));
         app.highlightDays()
     }) 
 
-
-
-
+    //Function to cycle through previous months
     $('.cycleBackwards').on('click', function (e){
 
         let currentMonthIndex = app.monthLookup.indexOf(app.currentMonth)
         let prevMonthIndex = currentMonthIndex - 1;
-
+        //Allow user to cycle from index 0(January) to index 11(December) 
         if (prevMonthIndex < 0) {
             prevMonthIndex = 11;
         }
@@ -208,12 +208,12 @@ app.init = function() {
         app.loadMonth(prevMonth)
     })
 
-
+    //Function to cycle through next months
     $('.cycleForwards').on('click', function (e) {
 
         let currentMonthIndex = app.monthLookup.indexOf(app.currentMonth)
         let nextMonthIndex = currentMonthIndex + 1;
-
+        //Allow user to cycle from index 0(January) to index 11(December)
         if (nextMonthIndex > 11) {
             nextMonthIndex = 0;
         }
@@ -224,8 +224,6 @@ app.init = function() {
         $('.calendar').removeClass('removeBottomBorder')
         app.loadMonth(nextMonth)
     })
-
-    
 }
 
 $(function() {
